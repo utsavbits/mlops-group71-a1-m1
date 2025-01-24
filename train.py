10737c0a767e9b670ccd5df0764c27ed909c44df
Utsav Priyadarshi <Utsav.Priyadarshi@vanderlande.com>
2025-01-24 18:43:48 +0530

    New changes related Makefile for CML

--- train.py
+++ train.py
@@ -1,5 +1,76 @@
+### Loading the Dataset############################
 import pandas as pd
 
 drug_df = pd.read_csv("Data/drug.csv")
 drug_df = drug_df.sample(frac=1)
 drug_df.head(3)
+
+### Train Test Split################################
+from sklearn.model_selection import train_test_split
+
+X = drug_df.drop("Drug", axis=1).values
+y = drug_df.Drug.values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=125
+)
+
+### Machine Learning Pipelines########################
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+
+cat_col = [1,2,3]
+num_col = [0,4]
+
+transform = ColumnTransformer(
+    [
+        ("encoder", OrdinalEncoder(), cat_col),
+        ("num_imputer", SimpleImputer(strategy="median"), num_col),
+        ("num_scaler", StandardScaler(), num_col),
+    ]
+)
+pipe = Pipeline(
+    steps=[
+        ("preprocessing", transform),
+        ("model", RandomForestClassifier(n_estimators=100, random_state=125)),
+    ]
+)
+pipe.fit(X_train, y_train)
+
+### Model Evaluation##############################################
+### Evaluate the performance of the model by calculating both the accuracy and F1 score.####
+from sklearn.metrics import accuracy_score, f1_score
+
+predictions = pipe.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+f1 = f1_score(y_test, predictions, average="macro")
+
+print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
+
+
+### Create the metrics file and save it in the Results folder.#########
+with open("Results/metrics.txt", "w") as outfile:
+    outfile.write(f"\nAccuracy = {round(accuracy,2)}, F1 Score = {round(f1,2)}.")
+
+
+### create the confusion matrix and save the image file into the Results folder.########
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+
+cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
+disp.plot()
+plt.savefig("Results/model_results.png", dpi=120)
+
+
+### Saving the Model
+import joblib
+
+# Save the pipeline
+joblib.dump(pipe, "Model/drug_pipeline.joblib")
+
+# Load the pipeline
+pipe = joblib.load("Model/drug_pipeline.joblib")
