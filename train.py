a42c29f590d3d8c186f8a504b2086f48ca8773b1
Utsav <2023aa05358@wilp.bits-pilani.ac.in>
2025-01-25 13:22:21 +0530

    Update train.py for linting issues

--- train.py
+++ train.py
@@ -1,13 +1,20 @@
-### Loading the Dataset############################
+### Loading the Dataset
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.metrics import accuracy_score, f1_score, ConfusionMatrixDisplay, confusion_matrix
+import matplotlib.pyplot as plt
+import joblib
 
 drug_df = pd.read_csv("Data/drug.csv")
 drug_df = drug_df.sample(frac=1)
 drug_df.head(3)
 
-### Train Test Split################################
-from sklearn.model_selection import train_test_split
-
+### Train Test Split
 X = drug_df.drop("Drug", axis=1).values
 y = drug_df.Drug.values
 
@@ -15,13 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=125
 )
 
-### Machine Learning Pipelines########################
-from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OrdinalEncoder, StandardScaler
-
+### Machine Learning Pipelines
 cat_col = [1,2,3]
 num_col = [0,4]
 
@@ -40,10 +41,8 @@ pipe = Pipeline(
 )
 pipe.fit(X_train, y_train)
 
-### Model Evaluation##############################################
-### Evaluate the performance of the model by calculating both the accuracy and F1 score.####
-from sklearn.metrics import accuracy_score, f1_score
-
+### Model Evaluation
+### Evaluate the performance of the model by calculating both the accuracy and F1 score.
 predictions = pipe.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 f1 = f1_score(y_test, predictions, average="macro")
@@ -51,24 +50,18 @@ f1 = f1_score(y_test, predictions, average="macro")
 print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
 
 
-### Create the metrics file and save it in the Results folder.#########
+### Create the metrics file and save it in the Results folder.
 with open("Results/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {round(accuracy,2)}, F1 Score = {round(f1,2)}.")
 
 
-### create the confusion matrix and save the image file into the Results folder.########
-import matplotlib.pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-
+### create the confusion matrix and save the image file into the Results folder.
 cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
 disp.plot()
 plt.savefig("Results/model_results.png", dpi=120)
 
-
-### Saving the Model
-import joblib
-
+# Saving the Model
 # Save the pipeline
 joblib.dump(pipe, "Model/drug_pipeline.joblib")
 
